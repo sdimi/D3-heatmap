0d36b23100f309c214242767c11a5a77ff1f6dc7
Dimitris Spathis <dispathis@gmail.com>
2025-07-06 11:11:39 +0100

    Update s4.js

--- js/s4.js
+++ js/s4.js
@@ -10,7 +10,7 @@ var margin = { top: 50, right: 0, bottom: 100, left: 60 },
 
 
 
-      d3.csv("http://communitypoprefs.com/data/season4.csv",
+      d3.csv("./data/season4.csv",
         function(d) {
          // var strings = d3.tsv.parse(string);
           return {
